736a35ed0c08095402a0e7c741f6d1863a48c066
Altie122 <77561831+altie122@users.noreply.github.com>
2024-10-09 13:48:03 -0600

    fix build?

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,21 +38,6 @@ const config: Config = {
       },
     ],
     [
-      "@docusaurus/plugin-sitemap",
-      {
-        lastmod: "date",
-        changefreq: "weekly",
-        priority: 0.5,
-        ignorePatterns: ["/tags/**"],
-        filename: "sitemap.xml",
-        createSitemapItems: async (params) => {
-          const { defaultCreateSitemapItems, ...rest } = params;
-          const items = await defaultCreateSitemapItems(rest);
-          return items.filter((item) => !item.url.includes("/page/"));
-        },
-      },
-    ],
-    [
       "@docusaurus/plugin-pwa",
       {
         debug: true,
